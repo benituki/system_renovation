8286b13dbd196c4c0326c60198dc26fd9fe72816
shiori123 <shioo5555@gmail.com>
2024-01-17 16:50:38 +0900

    デバックバーを追加（環境設定。）

--- AtlasManagementSystem-master/config/app.php
+++ AtlasManagementSystem-master/config/app.php
@@ -162,6 +162,9 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        // デバックバー追加
+        Barryvdh\Debugbar\ServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
@@ -225,6 +228,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // デバックバーを追加
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
